36c29b420270d9adda251485fc9ec2bec513e65d
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-10 03:25:50 -0500

    Updates API URL to production
    Changes the API URL from localhost to the production server.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://maicol1.pagegear.co'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://maicol1.pagegear.co'
+  // apiUrl: 'http://localhost:8080'
 };
